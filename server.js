fed722794572209faa7622ba577e943a2e73f441
Tanner Houghton <houta483@uchicago.edu>
2020-05-19 19:44:10 -0500

    testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const PORT = 3306;
+const PORT = 3307;
 
 app.use(cookieParser())
 app.use(bodyParser.json())
